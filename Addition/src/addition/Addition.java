4aa82a03f4773855c48f1021fcd039033523fea9
montanabraswell <montanabraswell1@gmail.com>
2017-11-06 21:25:47 -0500

    Add files via upload
    Practicing some ez addition and printing them out

---
+++ Addition/src/addition/Addition.java
@@ -0,0 +1,44 @@
+/* MOOC Java Tutorial 6
+ * Java Tutorial 6
+ * Montana Braswell
+ * 11/2/2017
+ */
+package addition;
+
+/**
+ *
+ * @author Montana
+ */
+public class Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x;
+        int y;
+        int answer;;
+    
+        
+        x = 5;
+        y = 4;
+        answer = x + y;
+        System.out.print( x );
+        System.out.println("+"+ y + "=" + answer);
+       
+        
+        
+        
+        
+        int a;
+        int b;
+        int result;
+        
+        a = 73457;
+        b = 12888;
+        result = a + b;
+        System.out.println(a + " + "  + b +  " = " + result);
+    }
+    
+}
